799525fcba1a01752941942d63fcec0a9a84bff4
mikanfactory <k952i4j14x17@gmail.com>
2025-09-04 22:13:32 +0900

    Add environment configuration and update .gitignore

---
+++ backend/src/papercast/config.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENV = os.getenv("ENV", "development")
+
+if ENV == "production":
+    GOOGLE_CLOUD_STORAGE_BUCKET = os.getenv("GOOGLE_CLOUD_PRODUCTION_STORAGE_BUCKET")
+else:
+    GOOGLE_CLOUD_STORAGE_BUCKET = os.getenv("GOOGLE_CLOUD_DEVELOPMENT_STORAGE_BUCKET")
+
+GOOGLE_CLOUD_LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
+GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+if ENV == "production":
+    SUPABASE_PROJECT_URL = os.getenv("SUPABASE_PROJECT_URL")
+    SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
+else:
+    SUPABASE_PROJECT_URL = os.getenv("SUPABASE_LOCAL_PROJECT_URL")
+    SUPABASE_API_KEY = os.getenv("SUPABASE_LOCAL_API_KEY")
+
+SUPABASE_LOCAL_PROJECT_URL = os.getenv("SUPABASE_LOCAL_PROJECT_URL")
+SUPABASE_LOCAL_API_KEY = os.getenv("SUPABASE_LOCAL_API_KEY")
